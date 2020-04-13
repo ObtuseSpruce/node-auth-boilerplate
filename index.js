13502f5695dd5e5695d65ac5c96382796b8a1fb0
ObtuseSpruce <williamllapitan@gmail.com>
2020-04-13 10:33:22 -0700

    implemented homepage and created nav bar content

---
+++ index.js
@@ -0,0 +1,45 @@
+//********************************
+//    NODE MODULES
+//********************************
+//require needed modules
+let express = require('express')
+//add layouts
+let layouts = require('express-ejs-layouts')
+//create an app instance
+let app = express()
+
+
+//********************************
+//    SETTINGS / MIDDLEWARE
+//********************************
+
+//set template lang to ejs
+app.set('view engine', 'ejs')
+
+//tell express to use layouts
+app.use(layouts)
+
+// set up a static folder
+app.use(express.static('static'))
+
+//********************************
+//           ROUTES
+//********************************
+//home page
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+//create a wildcard (catch-all) route
+//this will catch everything. Think like a 404 error page.
+//this NEEDS to be last
+app.get('*', (req, res) => {
+    res.render('error')
+})
+
+
+//********************************
+//    listener
+//********************************
+//pick a port to listen on
+app.listen(3000)
